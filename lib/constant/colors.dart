3a20ae8ce94497afc0418066c0f14bbb52adba89
rofidamedhat <rofidamedhat2@gmail.com>
2024-07-09 16:59:41 +0300

    first commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+var w = const Color(0xFFEEEFF5);
+var bl = const Color(0xFF3A3A3A);
+var r = const Color(0xFFDA4040);
+var bur = const Color(0xFF5F52EE);
+var br = const Color(0xFF3A3A3A);
+var bag = const Color(0xFF717171);
